0c6d9a87a2c830e6e0226fe6903073d0f18b2ed5
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-10-03 16:33:49 +0000

    Deploying to gh-pages from @ prorochenko/goit-js-hw-08@cd2e8ff67edbd5f8903f79e443b78b6ac5bd531c 🚀

--- 02-video.5d321fc1.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.on("pause")}();
-//# sourceMappingURL=02-video.5d321fc1.js.map
--- 02-video.81ef2afa.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e);o.on("play",(function(){console.log("played the video!")})),o.getVideoTitle().then((function(e){console.log("title:",e)})),o.on("pause");
-//# sourceMappingURL=02-video.81ef2afa.js.map
