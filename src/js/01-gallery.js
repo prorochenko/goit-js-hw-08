968f5df8101c8ed5a86652d1951ad41e23e70a50
prorochenko <33101252+prorochenko@users.noreply.github.com>
2022-10-03 13:34:57 +0300

    task three

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,25 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
 
-test;
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>`;
+  })
+  .join(``);
+
+gallery.insertAdjacentHTML('afterbegin', markup);
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector(`.feedback-form`),
+  textarea: document.querySelector(`.feedback-form textarea`),
+  email: document.querySelector(`.feedback-form input`),
+};
+
+const STORAGE_KEY = `feedback-form-state`;
+const feedbackForm = {};
+
+refs.form.addEventListener(`submit`, onFormSubmit);
+refs.textarea.addEventListener(`input`, throttle(onTextareaInput, 500));
+refs.email.addEventListener(`input`, throttle(onTextareaInput, 500));
+
+//localStorage write in input, if it have.
+dataFromLocalStorage();
+
+//save in local storage what client write
+function onTextareaInput(evt) {
+  feedbackForm[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
+}
+
+//btn submit - reset storage
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+//localStorage write in input, if it have.
+function dataFromLocalStorage() {
+  const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (savedMessage) {
+    refs.textarea.value = savedMessage.message;
+    refs.email.value = savedMessage.email;
+  }
+}
